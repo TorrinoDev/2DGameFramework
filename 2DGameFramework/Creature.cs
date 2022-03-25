8252861d601aa2ab87c38b38f6ad44195e88f800
Malte <70883688+MalteZea@users.noreply.github.com>
2022-03-25 12:21:35 +0100

    Add project files.

---
+++ 2DGameFramework/Creature.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DGameFramework
+{
+    public class Creature
+    {
+        public int Hitpoints { get; set; }
+        public string CreatureName { get; set; }
+
+        public void Attack<T>(T enemy)
+        {
+            //Implement body
+        }
+
+        public void RecieveDamage(int damage)
+        {
+            //Implement body
+        }
+
+        public void Loot<T>(T lootableObject)
+        {
+            //Implement body
+        }
+    }
+}
---
+++ 2DGameFramework/WorldObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DGameFramework
+{
+    public class WorldObject
+    {
+        public bool Lootable { get; set; }
+        public string ObjectName { get; set; }
+        public bool Removable { get; set; }
+    }
+}
